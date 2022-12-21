f3f9fc50facb2b2a66b1ea008ac15528847c2ffd
Patrick Mason <patrickmason73@gmail.com>
2022-12-20 22:27:39 -0700

    commit tree 0eccebe33223cc556ab42c3704bbc502f8ab2920

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+const users = {
+    name: "Steve",
+    email: "steve@steve.com",
+}
+
+
+const newObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(users),
+}
+
+const list = document.createElement('li')
+
+function appendData(data) {
+    
+    
+   list.append(data)
+}
+
+
+function submitData() {
+    return  fetch('http://localhost:3000/users', newObject)
+    .then(res => res.json())
+    .then(data => {
+        let id = data.id
+        let body = document.querySelector('body')
+        body.append(id)
+    })
+    .catch(error => {
+        let body = document.querySelector('body')
+        body.append(error.message)
+    })
+}
+
+
+
+
+
+submitData();
